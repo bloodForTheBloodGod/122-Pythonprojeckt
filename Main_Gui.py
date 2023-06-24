ee9bf70184f9200899fa315806a3e390c15144b6
Jérôme Henkler <jerome.henkler@inf.local>
2023-06-24 17:27:19 +0200

    functioning database display

--- Main_Gui.py
+++ Main_Gui.py
@@ -1,36 +1,25 @@
-from tkinter import *
+
 from tkinter import ttk
 import ctypes
 from wh40k import  *
-win = Tk()
-win.title("Mein Fenster")
-from functools import partial
-# Bildschirmgröße abrufen
-user32 = ctypes.windll.user32
-screen_width = user32.GetSystemMetrics(0)
-screen_height = user32.GetSystemMetrics(1)
+root = tk.Tk()
 
-# Fenstergröße einstellen
-window_width = int(screen_width * 0.8)  # 80% der Bildschirmbreite
-window_height = int(screen_height * 0.8)  # 80% der Bildschirmhöhe
-x_position = int((screen_width - window_width) / 2)  # Zentriert auf dem Bildschirm
-y_position = int((screen_height - window_height) / 2)
+# Create five buttons
+abilitiesButton = tk.Button(root, text="abilities", command=lambda: open_data_window("select * from abilities"))
+factionButton = tk.Button(root, text="faction", command=lambda: open_data_window("select * from faction"))
+keywordButton = tk.Button(root, text="keywords", command=lambda: open_data_window("select * from keywords"))
+statsheetsButton = tk.Button(root, text="statsheet", command=lambda: open_data_window("SELECT * FROM statsheet join units on statsheet.id = units.id"))
+unitButton = tk.Button(root, text="units", command=lambda: open_data_window("select * from units"))
+weaponButton = tk.Button(root, text="weapons", command=lambda: open_data_window("select * from weapons"))
 
-win.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
-button1 = Button(win, text="weapons", command=partial(openNewWindow,win))
-button1.place(x=100, y=100)
+# Pack the buttons
+abilitiesButton.pack()
+factionButton.pack()
+keywordButton.pack()
+statsheetsButton.pack()
+unitButton.pack()
+weaponButton.pack()
 
-button2 = Button(win, text="abilities", command=partial(openNewWindow,win))
-button2.place(x=200, y=100)
-
-button3 = Button(win, text="factions", command=partial(openNewWindow,win))
-button3.place(x=300, y=100)
-
-button4 = Button(win, text="keywords", command=partial(openNewWindow,win))
-button4.place(x=400, y=100)
-
-button5 = Button(win, text="statsheet", command=partial(openNewWindow,win))
-button5.place(x=500, y=100)
-
-win.mainloop()
+# Start the main tkinter event loop
+root.mainloop()
--- wh40k.py
+++ wh40k.py
@@ -1,24 +1,104 @@
 import mysql.connector
-from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
-cnx = mysql.connector.connect(user='root', password='root',
+db = mysql.connector.connect(user='root', password='root',
                               host='localhost',
                               database='warhammer40k')
-cnx.close()
+
+# Variable to keep track of the currently open cell window
+current_cell_window = None
 
 
-def openNewWindow(win):
-    # Toplevel object which will
-    # be treated as a new window
-    newWindow = Toplevel(win)
+def fetch_data(button_query):
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-    # sets the title of the
-    # Toplevel widget
-    newWindow.title("New Window")
+    # Execute a query to fetch all data from the dynamically constructed table
+    cursor.execute(button_query)
 
-    # sets the geometry of toplevel
-    newWindow.geometry("200x200")
+    # Fetch all the data
+    data = cursor.fetchall()
 
-    # A Label widget to show in toplevel
-    Label(newWindow,
-          text="This is a new window").pack()
+    # Close the cursor
+    cursor.close()
+
+    # Return the fetched data
+    return data
+
+
+def open_data_window(button_value):
+    # Create a new window
+    data_window = tk.Toplevel()
+
+    # Create a Canvas widget
+    canvas = tk.Canvas(data_window)
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    # Create a Scrollbar widget for vertical scrolling
+    scrollbar_y = ttk.Scrollbar(data_window, orient=tk.VERTICAL, command=canvas.yview)
+    scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y)
+
+    # Create a Scrollbar widget for horizontal scrolling
+    scrollbar_x = ttk.Scrollbar(data_window, orient=tk.HORIZONTAL, command=canvas.xview)
+    scrollbar_x.pack(side=tk.BOTTOM, fill=tk.X)
+
+    # Create a Frame inside the Canvas for scrollable content
+    frame = ttk.Frame(canvas)
+
+    # Add the Frame to the Canvas
+    canvas.create_window((0, 0), window=frame, anchor=tk.NW)
+
+    # Fetch the data
+    data = fetch_data(button_value)
+
+    # Create labels for column names
+    for j, column_name in enumerate(data[0]):
+        label = ttk.Label(frame, text=column_name)
+        label.grid(row=0, column=j, padx=5, pady=5, sticky='nsew')
+
+    # Insert data into labels
+    for i, row in enumerate(data[0:]):  # Exclude the first row
+        for j, value in enumerate(row):
+            label = ttk.Label(frame, text=value)
+            label.grid(row=i, column=j, padx=5, pady=5, sticky='nsew')
+            label.bind('<Button-1>', lambda event, row=i, column=j: show_cell_content(row, column, data))
+
+    # Configure the Scrollbars to work with the Canvas and Frame
+    scrollbar_y.configure(command=canvas.yview)
+    scrollbar_x.configure(command=canvas.xview)
+    canvas.configure(yscrollcommand=scrollbar_y.set, xscrollcommand=scrollbar_x.set)
+
+    # Function to configure the Canvas scroll region based on the Frame size
+    def configure_canvas(event):
+        canvas.configure(scrollregion=canvas.bbox('all'))
+
+    # Bind the configure_canvas function to the Frame size change event
+    frame.bind('<Configure>', configure_canvas)
+
+    # Start the Tkinter event loop for the data_window
+    data_window.mainloop()
+
+
+def show_cell_content(row, column, data):
+    global current_cell_window
+
+    # Check if there is already a cell window open
+    if current_cell_window is not None:
+        current_cell_window.destroy()
+
+    # Create a new window
+    cell_window = tk.Toplevel()
+
+    # Assign the new window to the current_cell_window variable
+    current_cell_window = cell_window
+
+    # Get the content of the clicked cell
+    cell_content = data[row][column]
+
+    # Create a Label to display the cell content
+    label = tk.Label(cell_window, text=cell_content)
+    label.pack(padx=10, pady=10)
+
+    # Start the Tkinter event loop for the cell_window
+    cell_window.mainloop()
